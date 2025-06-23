0846adaa46d42bd44532fa04e647262c542111d7
janakinzel <jana.kinzel@uni-ulm.de>
2025-06-23 09:26:29 +0200

    Added analyze_simu and many_simu_steps

---
+++ analyze_simu.R
@@ -0,0 +1,28 @@
+library(survival)
+
+# low-dimensional analysis: standard Cox model on every transition separately
+# goal: see whether the "right" non-zero variables for each transition are found in low-dim
+
+# sim: a dataset resulting from the function simulate_IDM_lowdim (containing the transition times and the covariates with non-zero effect on any transition)
+analyze_simu <- function(sim){
+  # Cox models on subsets
+  covariates <- setdiff(names(sim), c("id","from","to","entry","exit"))
+  formula_to1 <- as.formula(paste("Surv(entry,exit,to==1)~", paste(covariates,collapse="+")))
+  formula_to2 <- as.formula(paste("Surv(entry,exit,to==2)~", paste(covariates,collapse="+")))
+  
+  cox01 <- coxph(formula_to1,sim,subset=from==0) # transition 0 -> 1
+  cox02 <- coxph(formula_to2,sim,subset=from==0) # transition 0 -> 2
+  cox12 <- coxph(formula_to2,sim,subset=from==1) # transition 1 -> 2
+  
+  # extract covariates with significant effects
+  coef01 <- as.data.frame(summary(cox01)$coefficients)
+  coef02 <- as.data.frame(summary(cox02)$coefficients)
+  coef12 <- as.data.frame(summary(cox12)$coefficients)
+  
+  sign01 <- ifelse(coef01$`Pr(>|z|)`<0.05,1,0)
+  sign02 <- ifelse(coef02$`Pr(>|z|)`<0.05,1,0)
+  sign12 <- ifelse(coef12$`Pr(>|z|)`<0.05,1,0)
+  
+  
+  return(list(sign01,sign02,sign12))
+}
---
+++ many_simu_steps.R
@@ -0,0 +1,20 @@
+# nsim: number of simulation steps
+# n: number of individuals
+# bh: list of constant baseline hazard for each transition
+# betas: list with vector of coefficients for each transition
+# X_lowdim: covariates with non-zero effect on any transition, nrow=n, ncol=length of beta vectors
+many_simu_steps <- function(nsim,n,bh,betas){
+  count_nonzero01 <- rep(0,length(betas[[1]]))
+  count_nonzero02 <- rep(0,length(betas[[1]]))
+  count_nonzero12 <- rep(0,length(betas[[1]]))
+  
+  for(i in 1:nsim){
+    X_lowdim <- matrix(rnorm(100*9),nrow=100)
+    sim <- simulate_IDM_lowdim(n,bh,betas,X_lowdim)
+    nonzero <- analyze_simu(sim)
+    count_nonzero01 <- count_nonzero01 + nonzero[[1]]
+    count_nonzero02 <- count_nonzero02 + nonzero[[2]]
+    count_nonzero12 <- count_nonzero12 + nonzero[[3]]
+  }
+  return(list(count_nonzero01/nsim,count_nonzero02/nsim,count_nonzero12/nsim))
+}
